5db00deec7b61d76cbe9797427ca66a576cfd10d
AntMouse <antmouse@naver.com>
2025-02-15 10:57:56 +0900

    프론트엔드 데이터 임시 삭제

--- frontend/src/Components/js/utils.js
+++
@@ -1,8 +0,0 @@
-// components/js/utils.js
-export const maskPhoneNumber = (phoneNumber) => {
-  if (phoneNumber.length >= 4) {
-    const maskedPart = phoneNumber.slice(-4).replace(/./g, '*');
-    return phoneNumber.slice(0, -4) + maskedPart;
-  }
-  return phoneNumber;
-};
--- frontend/src/setupTests.js
+++
@@ -1,5 +0,0 @@
-// setupTests.js
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
